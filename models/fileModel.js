de2dac3407cf62c7163024d3efd3781c8f957054
tejasaher1 <tejasaher9623@gmail.com>
2024-06-19 10:05:47 +0530

    Adding Cloud MongoDB and making application live

--- models/fileModel.js
+++ models/fileModel.js
@@ -1,40 +1,49 @@
-const { timeStamp } = require('console');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const { type } = require('os');
-const path = require('path');
-const AVATAR_PATH = path.join('/uploads/All_file_stored');
+const { timeStamp } = require("console");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const { type } = require("os");
+const path = require("path");
+const AVATAR_PATH = path.join("/uploads/All_file_stored");
+const fs = require("fs");
 
-const csvFileSchema = mongoose.Schema({
-    fileName : {
-        type: String,
-        require : true
+const csvFileSchema = mongoose.Schema(
+  {
+    fileName: {
+      type: String,
+      require: true,
     },
     filePath: {
-        type: String,
-        require : true
-    }
-},{
-    timestamps: true
-});
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 if (!fs.existsSync(AVATAR_PATH)) {
-    fs.mkdirSync(AVATAR_PATH, { recursive: true });
+  fs.mkdirSync(AVATAR_PATH, { recursive: true });
 }
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '..', AVATAR_PATH));
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 // Static method to handle file uploads
-csvFileSchema.statics.uploadAvatar = multer({ storage: storage }).single('csvFile');
+csvFileSchema.statics.uploadAvatar = multer({ storage: storage }).single(
+  "csvFile"
+);
 csvFileSchema.statics.avatarPath = AVATAR_PATH;
 
-const File = mongoose.model('csvFile', csvFileSchema);
+const File = mongoose.model("csvFile", csvFileSchema);
 
 module.exports = File;
