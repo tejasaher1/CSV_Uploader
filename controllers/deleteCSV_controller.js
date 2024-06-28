7666915f23f491c46bc33ddf0bb547776575c46c
tejasaher1 <97150758+tejasaher1@users.noreply.github.com>
2024-06-28 21:40:44 +0530

    Update deleteCSV_controller.js
    Adding comment's

--- controllers/deleteCSV_controller.js
+++ controllers/deleteCSV_controller.js
@@ -2,14 +2,20 @@ const fileModel = require("../models/fileModel");
 const path = require("path");
 const fs = require("fs");
 
+//This controller is work to delete file from DB and also unlink the file from uploads path using fs
+
 module.exports.deleteCSVFile = async function (req, res) {
+  // Get the file ID from the query parameters
   const fileID = req.query.id;
   const file = await fileModel.findById(fileID);
+  
+  // Use fs.unlink to delete the file from the file system
   fs.unlink(path.join(__dirname, "..", file.filePath), function (error) {
     if (error) {
       console.log("Error in deleting file", error);
     }
 
+    // Find the file in the database by ID and delete it
     fileModel
       .findByIdAndDelete(fileID)
       .then(function (deletedFile) {
@@ -18,7 +24,7 @@ module.exports.deleteCSVFile = async function (req, res) {
       })
       .catch(function (err) {
         console.error("Error deleting file:", err);
-        return;
+        return res.redirect("back");
       });
   });
 };
